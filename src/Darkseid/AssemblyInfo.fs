8889bcda34e4c6303e2046cf1c96af4faaa7ddc6
Dustin Moris Gorski <dustinmoris@gmail.com>
2016-02-11 10:52:42 +0000

    Assembly version change after fresh re-build.

--- src/Darkseid/AssemblyInfo.fs
+++ src/Darkseid/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Darkseid")>]
 [<assembly: AssemblyProductAttribute("Darkseid")>]
 [<assembly: AssemblyDescriptionAttribute("Actor-based library to help you build a real-time data producing application on top of Amazon Kinesis")>]
-[<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
+[<assembly: AssemblyVersionAttribute("0.3.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0.0"
+    let [<Literal>] Version = "0.3.0"
